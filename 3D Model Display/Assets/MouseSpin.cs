5d51b5c4902651af0e3db1e8297e6b6db3cbc373
Jordan Chew <kungfusnake@gmail.com>
2017-11-06 20:58:32 -0800

    Updated moving block, works with mouse

---
+++ 3D Model Display/Assets/MouseSpin.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class MouseSpin : MonoBehaviour {
+
+    Vector3 mouseStart;
+    Vector3 rotateVec;
+    int slowdown;
+
+    void Start()
+    {
+        mouseStart = Input.mousePosition;
+        rotateVec = Vector3.zero;
+        slowdown = 250;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseStart = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(0)) {
+            rotateVec = ((mouseStart - Input.mousePosition) / Time.deltaTime)/slowdown;
+            mouseStart = Input.mousePosition;
+        }
+        transform.Rotate(rotateVec, Space.World);
+        rotateVec = rotateVec * (0.9f);
+    } 
+}
