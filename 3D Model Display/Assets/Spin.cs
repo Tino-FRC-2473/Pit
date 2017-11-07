bce04015c5c9fd35f9943b4c4403ef9a42327ac1
Jordan Chew <kungfusnake@gmail.com>
2017-11-06 19:47:39 -0800

    Added dummy Unity project for Sahil to test with

---
+++ 3D Model Display/Assets/Spin.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Spin : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey("a"))
+        {
+            transform.Rotate(Vector3.down, Space.World);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Rotate(Vector3.up, Space.World);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Rotate(Vector3.left, Space.World);
+        }
+        if (Input.GetKey("w"))
+        {
+            transform.Rotate(Vector3.right, Space.World);
+        }
+    }
+}
